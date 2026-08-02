fffba36ce0d91f62e78ccc9682f8b00a4a502e8e
Emmanuel <ecobian12310@gmail.com>
2026-08-01 18:33:30 -0700

    changed to match prisma schema

--- backend/src/validations/authSchema.js
+++ backend/src/validations/authSchema.js
@@ -1,8 +1,8 @@
 const Joi = require('joi');
 
 const authSchema = Joi.object({
-  email: Joi.string().trim().lowercase().email().required(),
-  password: Joi.string().trim().required().max(72),
+  email: Joi.string().trim().lowercase().email().max(255).required(),
+  password: Joi.string().trim().required().max(72, 'utf8'),
 });
 
 module.exports = { authSchema };
