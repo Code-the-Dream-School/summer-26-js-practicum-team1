14965e38cec3f953b1e24ed301cad6b1304ba4ec
Archana Chavan <archana.chavan2@gmail.com>
2026-08-06 11:43:48 -0700

    Add registration route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
+import RegistrationPage from './pages/Register';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<RegistrationPage />} />
       </Route>
     </Routes>
   );
