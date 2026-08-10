1f6f0cbcbd1e5dd0468275f180d949da98c44f91
Archana Chavan <archana.chavan2@gmail.com>
2026-08-09 18:05:52 -0700

    Update App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import Login from './pages/Login';
 import SignupPage from './pages/SignupPage';
+import RequestorRegistration from './pages/Register';
 
 function App() {
   return (
@@ -13,7 +14,10 @@ function App() {
         <Route path="signup" element={<SignupPage />} />
         {/* Placeholder for future routes to volunteer registration and requestor registration pages */}
         {/* <Route path="volunteerRegistration" element={<VolunteerRegistration/>}/> */}
-        {/* <Route path="requesterRegistration" element={<RequestorRegistration/>}/> */}
+        <Route
+          path="requesterRegistration"
+          element={<RequestorRegistration />}
+        />
       </Route>
     </Routes>
   );
