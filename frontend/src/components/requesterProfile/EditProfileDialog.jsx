0e98825a71791a8ba98d47e10f27f2d3e697373d
Theera12 <aarthy.maaya@gmail.com>
2026-08-12 01:22:12 -0700

    added validation to all fields

--- frontend/src/components/requesterProfile/EditProfileDialog.jsx
+++ frontend/src/components/requesterProfile/EditProfileDialog.jsx
@@ -10,7 +10,7 @@ import {
   TextField,
 } from '@mui/material';
 
-import { useUpdateProfile } from '../hooks/requesterProfile/useUpdateProfile';
+import { useUpdateProfile } from '../../hooks/requesterProfile/useUpdateProfile';
 
 function EditProfileDialog({ open, onClose, formData }) {
   const updateProfile = useUpdateProfile();
