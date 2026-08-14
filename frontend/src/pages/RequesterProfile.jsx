ef8c9a127e77d3c08d71fbaa12756b76727dcd4b
Theera12 <aarthy.maaya@gmail.com>
2026-08-13 21:11:06 -0700

    refactored Profile field into seperate component

--- frontend/src/pages/RequesterProfile.jsx
+++ frontend/src/pages/RequesterProfile.jsx
@@ -27,6 +27,7 @@ import DoneIcon from '@mui/icons-material/Done';
 import { useGetProfile } from '../hooks/requesterProfile/useGetProfile';
 import { useUpdateProfileImage } from '../hooks/requesterProfile/useUpdateProfileImage';
 import EditProfileDialog from '../components/requesterProfile/EditProfileDialog';
+import ProfileField from '../components/requesterProfile/ProfileField';
 import { API_URL } from '../utils/constants';
 
 function RequesterProfile() {
@@ -305,32 +306,4 @@ function RequesterProfile() {
   );
 }
 
-function ProfileField({ icon, label, value }) {
-  return (
-    <Stack direction="row" spacing={2} sx={{ alignItems: 'flex-start' }}>
-      <Box sx={{ color: 'success.main', mt: 0.5 }}>{icon}</Box>
-
-      <Box>
-        <Typography
-          variant="body1"
-          color="text.primary"
-          sx={{ fontWeight: 700 }}
-        >
-          {label}
-        </Typography>
-
-        <Typography
-          variant="body1"
-          sx={{
-            color: 'text.secondary',
-            opacity: value ? 1 : 0.6,
-            fontStyle: value ? 'normal' : 'italic',
-          }}
-        >
-          {value?.trim() || 'Click Edit to add your information'}
-        </Typography>
-      </Box>
-    </Stack>
-  );
-}
 export default RequesterProfile;
