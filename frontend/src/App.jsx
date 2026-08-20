44ad1198102001d0df6b4f355558ba89ed572bce
cesarve-dev <cesar.radiate@gmail.com>
2026-08-20 01:20:07 -0700

    clean up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,11 @@ import VolunteerApprovals from './pages/admin/VolunteerApprovals';
 import AdminProtectedRoute from './components/auth/AdminProtectedRoute';
 import RequesterProtectedRoute from './components/auth/RequesterProtectedRoute';
 import UsersList from './pages/admin/UsersList';
-import AdminUserDetailPage from './pages/admin/AdminUserDetailPage';
 import Login from './pages/Login';
 import SignupPage from './pages/SignupPage';
 import VolunteerRegistrationPage from './pages/VolunteerRegistrationPage';
 import RequesterProfile from './pages/RequesterProfile';
 import RequestorRegistration from './pages/Register';
-import ProfilePage from './pages/ProfilePage';
-import ProtectedRoute from './components/auth/ProtectedRoute';
 
 function App() {
   return (
@@ -28,7 +25,6 @@ function App() {
           <Route path="admin/dashboard" element={<AdminDashboard />} />
           <Route path="admin/volunteers" element={<VolunteerApprovals />} />
           <Route path="admin/users" element={<UsersList />} />
-          <Route path="admin/users/:id" element={<AdminUserDetailPage />} />
         </Route>
         <Route path="signup" element={<SignupPage />} />
         <Route
@@ -40,10 +36,6 @@ function App() {
           {/* TODO: Move profile route inside requester dashboard once ready */}
           <Route path="profile" element={<RequesterProfile />} />
         </Route>
-        <Route
-          path="volunteerRegistration"
-          element={<VolunteerRegistrationPage />}
-        />
         {/* <Route path="requesterRegistration" element={<RequestorRegistration/>}/> */}
       </Route>
     </Routes>
