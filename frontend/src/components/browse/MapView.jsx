d387731636957811f1ca309e1d9b708fdc9e8925
Emmanuel <ecobian12310@gmail.com>
2026-08-27 09:08:02 -0700

    implemented map view components

---
+++ frontend/src/components/browse/MapView.jsx
@@ -0,0 +1,73 @@
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Tooltip,
+  useMap,
+} from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-cluster';
+import { useEffect } from 'react';
+import L from 'leaflet';
+import MapPopupCard from './MapPopupCard.jsx';
+import { categoryIcon } from '../../utils/browse.utils.js';
+
+const GEOAPIFY_API_KEY = import.meta.env.VITE_GEOAPIFY_API_KEY;
+
+function FitBounds({ requests, selectedLocation }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (requests.length === 0) {
+      if (selectedLocation) {
+        map.setView([selectedLocation.lat, selectedLocation.lon], 12);
+      }
+      return;
+    }
+
+    const bounds = L.latLngBounds(
+      requests.map((r) => [r.latitude, r.longitude])
+    );
+    if (selectedLocation) {
+      bounds.extend([selectedLocation.lat, selectedLocation.lon]);
+    }
+    map.fitBounds(bounds.pad(0.15));
+  }, [requests, selectedLocation, map]);
+
+  return null;
+}
+
+export default function MapView({ requests, selectedLocation, onSelectRequest }) {
+  return (
+    <MapContainer
+      center={[37.1662, -1194494]}
+      zoom={4}
+      style={{ height: 500, width: '100%', borderRadius: 8 }}
+    >
+      <TileLayer
+        url={`https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=${GEOAPIFY_API_KEY}`}
+        attribution='© OpenMapTiles © OpenStreetMap contributors'
+      />
+      <FitBounds requests={requests} selectedLocation={selectedLocation} />
+
+      <MarkerClusterGroup
+        spiderfyOnMaxZoom
+        spiderfyDistanceMultiplier={1.5}
+        showCoverageOnHover={false}
+        maxClusterRadius={40}
+      >
+        {requests.map((request) => (
+          <Marker
+            key={request.id}
+            position={[request.latitude, request.longitude]}
+            icon={categoryIcon()}
+            eventHandlers={{ click: () => onSelectRequest(request) }}
+          >
+            <Tooltip direction="top" offset={[0, -10]} opacity={1}>
+              <MapPopupCard request={request} />
+            </Tooltip>
+          </Marker>
+        ))}
+      </MarkerClusterGroup>
+    </MapContainer>
+  );
+}
